da697554d0ba7a0d7f5c07b4d88875f4fb78fbc9
Arrey Pearl <pearlarrey285@gmail.com>
2026-05-24 12:58:16 +0100

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,9 @@
+#Personal Bio Generator
+name = "Pearl"
+age = 19
+height = 1.69
+fav_tech = "Data Science"
+is_student = True
+print("My name is " + name + ".")
+print("I am " + str(age) + " years old.")
+print("My favorite tech field is " + fav_tech + ".")
